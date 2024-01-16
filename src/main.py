b2fa25b9a52c87ea474914f31f8d05bdf7fa537d
ksoczowka <ksoczowka.ks@gmail.com>
2024-01-16 07:19:28 +0100

    added simple command to check bot state and bot check for a specific channel now

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+token = open('token.txt').read()
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
@@ -15,9 +17,10 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Nigerundayou!')
-    if message.content.startswith('Czy '):
-        await message.channel.send(random.choice(['Tak', 'Nie']))
-        print(message.channel)
-client.run('MTE5NjExOTQzNjU1NDQyNDM3MQ.GbwUrv.NeR6AvmFd5PygSWxonsU_Bc5xkLHJT9xbB9c1o')
+    if str(message.channel) == 'joseph-joestar':
+        if message.content.startswith('Czy '):
+            choice = ['Tak', 'Nie']
+            await message.channel.send(random.choice(choice))
+        if message.content.startswith('TT'):
+            await message.channel.send(str(message.channel) + ' - ' + str(message.channel.id))
+client.run(token)
